cfb9c9fdbaed0e5bc4367713e8e2b148bfef2571
EmilioD0608 <emiliodelgadoladines2014@gmail.com>
2023-10-11 15:09:14 -0500

    Creacion del archivo fuente
    He creado el codigo fuente

---
+++ Proyecto.cpp
@@ -0,0 +1,27 @@
+// 1.Zona para las librerias
+#include<iostream>
+#include<string.h>
+#include<conio.h>
+#include<locale.h>
+#include<stdio.h>
+using namespace std;
+
+//2. Zona para constantes y variables globales
+
+
+
+
+//3. Zona para definir las funciones
+
+
+
+//4. Zona para la funcion principal
+int main() {
+	setlocale(LC_CTYPE,"Spanish");
+
+	getch();
+	return 0;
+}
+
+//5. Implementar las funciones
+
